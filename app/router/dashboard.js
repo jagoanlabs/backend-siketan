b4f4b3ce321e4423cde501c44dbfe7dbcf4ec376
Nasrul Fahmi <75nasrulfahmi@gmail.com>
2025-09-09 09:12:16 +0700

    awdawdawd

--- app/router/dashboard.js
+++ app/router/dashboard.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { auth, hasPermission } = require('../../midleware/auth');
 const { getDashboardIndexData } = require('../controllers/dashboard');
+const { PERMISSIONS } = require('../../helpers/roleHelpers');
 
-router.get('/', getDashboardIndexData);
+router.get('/', auth, hasPermission(PERMISSIONS.DASHBOARD_INDEX), getDashboardIndexData);
 module.exports = router;
